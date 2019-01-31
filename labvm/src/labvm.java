a71b9fcee72e1826ae07c36a9fcda03a82642064
Pham Nguyen Quang <nguyenquang_pham@fb2.fra-uas.de>
2019-01-31 12:34:18 +0100

    some clean up

--- labvm/src/labvm.java
+++ labvm/src/labvm.java
@@ -13,16 +13,5 @@ public class labvm {
 	public static void main(String args[]) throws IOException {
 		GeneralVMHandler vmHandler = new virshHandler();
 		new OSSelectionGUI(vmHandler);
-//		XMLReadWrite testIO = new XMLReadWrite("/home/quang/virsh/.config/DefOS.xml");
-//		testIO.removeInterface();
-
-//		
-//		testIO.modifyDisk("/home/quang/test.img");
-
-//		CommandExecutor comd = new CommandExecutor();
-//		comd.startCommand("ls /home/quang");
-
-//		InterfaceHandler iface = new InterfaceHandler("/home/quang/DevOS.xml");
-//		iface.writeInterfaceToXML();
 	}
 }
